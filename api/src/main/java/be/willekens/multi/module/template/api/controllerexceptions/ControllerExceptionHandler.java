b194ae524734c14b1dcd8f873fa4b0d1eed07f6a
Astrid <astrid.pinckers@minfin.fed.be>
2021-04-20 19:05:39 +0200

    ControllerExceptionHandler implemented

--- api/src/main/java/be/willekens/multi/module/template/api/controllerexceptions/ControllerExceptionHandler.java
+++ api/src/main/java/be/willekens/multi/module/template/api/controllerexceptions/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package be.willekens.multi.module.template.api.controllerexceptions;
 
+import be.willekens.multi.module.template.infrastructure.exceptions.InvalidEmailException;
+import be.willekens.multi.module.template.infrastructure.exceptions.InvalidPhoneNumberException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,9 +21,15 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
 
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    protected void ArgumentValidator(IllegalArgumentException ex, HttpServletResponse response) throws IOException {
-        logger.error("Invalid argument used", ex);
+    @ExceptionHandler(InvalidEmailException.class)
+    protected void invalidEmailHandler(InvalidEmailException ex, HttpServletResponse response) throws IOException {
+        logger.error("Invalid email address submitted");
+        response.sendError(BAD_REQUEST.value(), ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidPhoneNumberException.class)
+    protected void invalidPhoneNumberHandler(InvalidPhoneNumberException ex, HttpServletResponse response) throws IOException {
+        logger.error("Attempt to create a contact person without a phone number");
         response.sendError(BAD_REQUEST.value(), ex.getMessage());
     }
 
