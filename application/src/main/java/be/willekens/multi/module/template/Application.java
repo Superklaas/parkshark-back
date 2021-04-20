cb41414eb2555fe848f4d966132688a78e3f4e9c
Rafaël Willekens <66566509+RafaelWill@users.noreply.github.com>
2021-04-20 09:25:11 +0200

    Initial commit

---
+++ application/src/main/java/be/willekens/multi/module/template/Application.java
@@ -0,0 +1,16 @@
+package be.willekens.multi.module.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan
+@EnableJpaRepositories
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ application/src/test/java/be/willekens/multi/module/template/SmokeTest.java
@@ -0,0 +1,12 @@
+package be.willekens.multi.module.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+
+    @Test
+    void contextSpringTest() {
+    }
+}
