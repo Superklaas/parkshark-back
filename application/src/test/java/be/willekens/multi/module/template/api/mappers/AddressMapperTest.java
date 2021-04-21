64aba760569ccc26239ba5b5709e90199fa15097
Astrid <astrid.pinckers@minfin.fed.be>
2021-04-21 16:05:17 +0200

    ME1 - minor fix

--- application/src/test/java/be/willekens/multi/module/template/api/mappers/AddressMapperTest.java
+++ application/src/test/java/be/willekens/multi/module/template/api/mappers/AddressMapperTest.java
@@ -1,20 +1,19 @@
 package be.willekens.multi.module.template.api.mappers;
 
+import be.willekens.multi.module.template.domain.models.parking_lot.Address;
+import be.willekens.multi.module.template.domain.models.parking_lot.PostalCode;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class AddressMapperTest {
 
+    @Autowired
     private AddressMapper addressMapper;
 
-    @Autowired
-    public AddressMapperTest(AddressMapper addressMapper) {
-        this.addressMapper = addressMapper;
-    }
 
     @Test
     void ifPostalCodeAlreadyExistsInDB_DBOneIsUsed(){
-
+//        addressMapper.address_to_receiveAddress(new Address("blubl","snurp",new PostalCode("njue","shmeu")));
     }
 
     @Test
