1a643ec695b4779ac689f5b07bed70f7500a3ff9
Klaas Decock <klaasdc@icloud.com>
2021-04-22 13:38:15 +0200

    STORY ME1 - duplicate email: catch exception in MemberService & test

--- application/src/test/java/be/willekens/multi/module/template/service/MemberServiceSpringTest.java
+++ application/src/test/java/be/willekens/multi/module/template/service/MemberServiceSpringTest.java
@@ -6,15 +6,22 @@ import be.willekens.multi.module.template.domain.models.parking_lot.PostalCode;
 import be.willekens.multi.module.template.domain.models.users.Account;
 import be.willekens.multi.module.template.domain.models.users.Member;
 import be.willekens.multi.module.template.domain.models.users.Role;
+import be.willekens.multi.module.template.infrastructure.exceptions.InvalidEmailException;
 import org.assertj.core.api.Assertions;
+import be.willekens.multi.module.template.domain.models.users.Role;
+import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.time.LocalDate;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class MemberServiceSpringTest {
@@ -24,46 +31,57 @@ class MemberServiceSpringTest {
 
 
     @Test
-    void createMember_whenPostalCodeAlreadyExists(){
+    void createMember_whenPostalCodeAlreadyExists() {
         Member member = memberFactory();
-        member.getAddress().setPostalCode(new PostalCode("1234","Buenos Aires"));
+        member.getAddress().setPostalCode(new PostalCode("1234", "Buenos Aires"));
         Member dbMember = memberService.createMember(member);
 
-        Assertions.assertThat(dbMember.getAddress().getPostalCode()).isEqualTo(member.getAddress().getPostalCode());
+        assertThat(dbMember.getAddress().getPostalCode()).isEqualTo(member.getAddress().getPostalCode());
     }
 
     @Test
-    void createMember_whenPostalCodeDoesNotAlreadyExistInDB(){
+    void createMember_whenPostalCodeDoesNotAlreadyExistInDB() {
         Member member = memberFactory();
-        member.getAddress().setPostalCode(new PostalCode("xxxx","Test town"));
+        member.getAddress().setPostalCode(new PostalCode("xxxx", "Test town"));
         Member dbMember = memberService.createMember(member);
 
-        Assertions.assertThat(dbMember.getAddress().getPostalCode()).isEqualTo(member.getAddress().getPostalCode());
+        assertThat(dbMember.getAddress().getPostalCode()).isEqualTo(member.getAddress().getPostalCode());
     }
 
     @Test
-    void createMember_whenPostalCodeAlreadyExistsInDBButCityNameIsDifferent_thenUsesCityInDatabase(){
+    void createMember_whenPostalCodeAlreadyExistsInDBButCityNameIsDifferent_thenUsesCityInDatabase() {
         Member member = memberFactory();
-        member.getAddress().setPostalCode(new PostalCode("1234","Test town"));
+        member.getAddress().setPostalCode(new PostalCode("1234", "Test town"));
         Member dbMember = memberService.createMember(member);
 
-        Assertions.assertThat(dbMember.getAddress().getPostalCode().getPostalCode()).isEqualTo(member.getAddress().getPostalCode().getPostalCode());
-        Assertions.assertThat(dbMember.getAddress().getPostalCode().getLabel()).isEqualTo("Buenos Aires");
+        assertThat(dbMember.getAddress().getPostalCode().getPostalCode()).isEqualTo(member.getAddress().getPostalCode().getPostalCode());
+        assertThat(dbMember.getAddress().getPostalCode().getLabel()).isEqualTo("Buenos Aires");
     }
 
-    private Member memberFactory(){
+    @Test
+    void createMember_whenEmailAlreadyExists() {
+        Member member = memberFactory();
+        member.getAddress().setPostalCode(new PostalCode("1234", "Test town"));
+        member.setAccount(new Account("ihsan@parkshark.be", "password", Role.MEMBER));
+        assertThatThrownBy(() -> memberService.createMember(member))
+                .isInstanceOf(InvalidEmailException.class)
+                .hasMessage("this email address already exists");
+    }
+
+    private Member memberFactory() {
         return new Member()
-                .setAccount(new Account("test@test.test","testtest", Role.MEMBER))
+                .setAccount(new Account("test@test.test", "testtest", Role.MEMBER))
                 .setFirstName("John")
                 .setLastName("Doe")
-                .setLicencePlate(new LicencePlate("xxx555","BE"))
+                .setLicencePlate(new LicencePlate("xxx555", "BE"))
                 .setPhoneNumber("011223344")
                 .setRegistrationDate(LocalDate.now())
                 .setAddress(new Address()
-                    .setStreetName("testystreet")
-                    .setStreetNumber("test")
+                        .setStreetName("testystreet")
+                        .setStreetNumber("test")
                 );
     }
 
-
 }
+
+
--- service/src/main/java/be/willekens/multi/module/template/service/MemberService.java
+++ service/src/main/java/be/willekens/multi/module/template/service/MemberService.java
@@ -4,8 +4,10 @@ import be.willekens.multi.module.template.domain.models.parking_lot.Address;
 import be.willekens.multi.module.template.domain.models.parking_lot.PostalCode;
 import be.willekens.multi.module.template.domain.models.users.Member;
 import be.willekens.multi.module.template.domain.repository.MemberRepository;
+import be.willekens.multi.module.template.infrastructure.exceptions.InvalidEmailException;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -22,7 +24,12 @@ public class MemberService {
     public Member createMember(Member member) {
         Address address = member.getAddress();
         address.setPostalCode(getPostalCodeByPostalCodeOrCreateNewOne(address.getPostalCode()));
-       return memberRepository.save(member);
+        try {
+            return memberRepository.save(member);
+        } catch (DataIntegrityViolationException e) {
+            throw new InvalidEmailException("this email address already exists");
+        }
+
     }
 
     public PostalCode getPostalCodeByPostalCodeOrCreateNewOne(PostalCode postalCode) {
